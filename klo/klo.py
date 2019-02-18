58ef03e1ca32cf6c15c1408b3f70049cb3f0e5ce
Michael G <m.gajda@setlog.com>
2019-02-18 19:38:42 +0100

    Add environment varible to set password
    Export KLO_KEY to configure the login for the bot

--- klo/klo.py
+++ klo/klo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from matrix_client.client import MatrixClient
+import os
 
 
 # Called when a message is recieved.
@@ -14,8 +15,9 @@ client = MatrixClient("https://matrix.org")
 # New user
 #token = client.register_with_password(username="foobar", password="monkey")
 
+
 # Existing user
-token = client.login(username="foss-ag_klo", password="justkloklo")
+token = client.login(username="foss-ag_klo", password=os.environ['KLO_PW'])
 
 #room = client.create_room("#klotest:matrix.org")
 room = client.join_room("#klotest:matrix.org")
