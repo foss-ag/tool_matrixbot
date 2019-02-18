6361cda4ac36cf952243578683e2e8a364577803
overflow <alexander2.becker@udo.edu>
2019-02-18 22:35:38 +0100

    script now uses feature handler. added examples as placeholders

--- klo/klo.py
+++ klo/klo.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 
+from FeatureExamples import FeatureExamples
+from FeatureHandler import FeatureHandler
 from matrix_client.client import MatrixClient
 import os
 
 
-# Called when a message is recieved.
-def on_message(room, event):
-    if event['type'] == "m.room.message":
-        if event['content']['msgtype'] == "m.text":
-            print("{0}: {1}".format(event['sender'], event['content']['body']))
-
-
 def main():
+    # connect to server and join room
     client = MatrixClient("https://matrix.org")
-
     token = client.login(username="foss-ag_klo", password=os.environ['KLO_PW'])
-
     room = client.join_room("#klotest:matrix.org")
-    room.add_listener(on_message)
-    client.start_listener_thread()
 
+    # create FeatureHandler
+    fh = FeatureHandler(room, client)
+    # add features to FeatureHandler that are called when the specified command is posted in the Matrix room
+    fh.add_feature("!echo", FeatureExamples.echo)
+    fh.add_feature("!CUPS", FeatureExamples.caps)
+
+    # run script until it's stopped manually
     while True:
         pass
 
