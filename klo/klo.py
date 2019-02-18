085d02c201c6ae8236a323f7ff1d581466b3d127
overflow <alexander2.becker@udo.edu>
2019-02-18 19:18:27 +0100

    added message that receives messages from chat, added listener to room, started listener thread

--- klo/klo.py
+++ klo/klo.py
@@ -2,8 +2,15 @@
 
 from matrix_client.client import MatrixClient
 
-client = MatrixClient("https://matrix.org")
 
+# Called when a message is recieved.
+def on_message(room, event):
+    if event['type'] == "m.room.message":
+        if event['content']['msgtype'] == "m.text":
+            print("{0}: {1}".format(event['sender'], event['content']['body']))
+
+
+client = MatrixClient("https://matrix.org")
 # New user
 #token = client.register_with_password(username="foobar", password="monkey")
 
@@ -13,3 +20,6 @@ token = client.login(username="foss-ag_klo", password="justklo")
 #room = client.create_room("#klotest:matrix.org")
 room = client.join_room("#klotest:matrix.org")
 room.send_text("Hello!")
+
+room.add_listener(on_message)
+client.start_listener_thread()
