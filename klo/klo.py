91c5cfd78b66dee7bf63a740249827914545acd7
overflow <alexander2.becker@udo.edu>
2019-02-20 13:55:22 +0100

    reformatted code and added find_room the feature handler

--- klo/klo.py
+++ klo/klo.py
@@ -7,19 +7,13 @@ import os
 from bs4 import BeautifulSoup
 import requests
 
-def find_room():
+
+def find_room(sender, message):
     html = requests.get("https://foss-ag.de/").text
     soup = BeautifulSoup(html, 'html.parser')
-    date = (
-        soup
-            .find(id="ag-termine")
-            .find_next_sibling("div")
-            .find("ul")
-            .find("li")
-            .get_text()
-            .strip()
-    )
+    date = soup.find(id="ag-termine").find_next_sibling("div").find("ul").find("li").get_text().strip()
     return date
+
 
 def main():
     # connect to server and join room
@@ -31,7 +25,7 @@ def main():
     fh = FeatureHandler(room, client)
     # add features to FeatureHandler that are called when the specified command is posted in the Matrix room
     fh.add_feature("!echo", FeatureExamples.echo)
-    fh.add_feature("!CUPS", FeatureExamples.caps)
+    fh.add_feature("!room", find_room)
 
     # run script until it's stopped manually
     while True:
